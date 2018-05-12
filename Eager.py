12aa45d166f4c0e324654a59634b80a499901026
Cerosus <beedrill99@gmail.com>
2018-05-11 20:00:24 -0700

    Employed regular expressions, legendary bias, and list feature

--- Eager.py
+++ Eager.py
@@ -23,21 +23,21 @@ def name_filter(text): #Filter properly enclosed terms from message into a list
     array1 = []
     array2 = []
     i = 0
-    j = 0
-    t1 = text
-    t2 = text
+
     while 1:
-        i = t1.find('[[',i)
-        if i == -1: #If no more [[ found
+        pos = text.find('[[',i)
+        if pos == -1: #If no more [[ found
+            i = 0
             break
-        array1.append([i,'s'])
-        i = i + 1 #Ignore previous sets
+        array1.append([pos,'s'])
+        i = pos + 1 #Ignore previous sets
     while 1:
-        j = t2.find(']]',j)
-        if j == -1: #If no more ]] found
+        pos = text.find(']]',i)
+        if pos == -1: #If no more ]] found
             break
-        array1.append([j,'e'])
-        j = j + 1 #Ignore previous sets
+        array1.append([pos,'e'])
+        i = pos + 1 #Ignore previous sets
+        
     array1.sort() #Arrange bracket groups in order of appearance
     
     for i in range(0,len(array1)-1): #Check each pair of brackets to see if it matches [[ ]]
@@ -49,69 +49,98 @@ def name_filter(text): #Filter properly enclosed terms from message into a list
     print(array2)
     return array2
 
+def list_more(times, data, album):
+    print(times)
+    times = int(times)
+    if times > 10:
+        times = 10
+    for i in range(0,times):
+        if data.json()['data'][i] == 'error':
+            return album
+        album = get_image(data.json()['data'][i], album)
+    return album
+        
 def scryfall(list1): #Search each bracketed term through Scryfall, obtain card images
     album = []
     errors = []
     for name in list1:
         data = requests.get('https://api.scryfall.com/cards/search?q=!'+name) #checks for exact match
         if data.json()['object'] == "error": #if no exact match, check for partial match
-            data = requests.get('https://api.scryfall.com/cards/search?q='+name)
+            data = requests.get('https://api.scryfall.com/cards/search?q='+ 'name:/\\b('+ name +')\\b/' \
+                                +" t:legendary (t:creature or t:planeswalker)") #check for unique
+            if data.json()['object'] == "error":
+                data = requests.get('https://api.scryfall.com/cards/search?q='+'name:/^('+ name +')/') #match from start
+                if data.json()['object'] == "error":
+                    data = requests.get('https://api.scryfall.com/cards/search?q='+ name)
+
         if data.json()['object'] == "error":
             errors.append(name)
             continue
-        result1 = data.json()['data'][0]
-        print(result1['layout'])
-    
-        if result1['layout'] == "meld": #Card has half-card on back, 'melds' with another
-            related_cards = result1['all_parts']
-            name1 = related_cards[0]['name']
-            name2 = related_cards[1]['name']
-            name3 = related_cards[2]['name']
-            link1 = related_cards[0]['uri']
-            page1 = requests.get(link1)
-            image1 = page1.json()['image_uris']['normal']
-            link2 = related_cards[1]['uri']
-            page2 = requests.get(link2)
-            image2 = page2.json()['image_uris']['normal']
-            link3 = related_cards[2]['uri']
-            page3 = requests.get(link3)
-            image3 = page3.json()['image_uris']['normal']
-            print(image1)
-            print(image2)
-            print(image3)
-            album.append(InputMediaPhoto(image1, caption = name1))
-            album.append(InputMediaPhoto(image2, caption = name2))
-            album.append(InputMediaPhoto(image3, caption = name3))
-        elif result1['layout'] == "transform": #Card has full card on back
-            faces = result1['card_faces']
-            name1 = faces[0]['name']
-            name2 = faces[1]['name']
-            image1 = faces[0]['image_uris']['normal']
-            image2 = faces[1]['image_uris']['normal']
-            print(image1)
-            print(image2)
-            album.append(InputMediaPhoto(image1, caption = name1))
-            album.append(InputMediaPhoto(image2, caption = name2))
+        list_op = name.find('l:')
+        #print(list_op)
+        if list_op != -1:
+            times = name[list_op + 2 : list_op + 4]
+            album = list_more(times, data, album) #find several results
         else:
-            image = result1['image_uris']['normal']
-            name = result1['name']
-            print(name)
-            print(image)
-            album.append(InputMediaPhoto(image, caption = name))
-    return [album, errors]
+            album = get_image(data.json()['data'][0], album)  #find first result
+
+    return (album, errors)
+
+def get_image(result,album):
+    print(result['layout'])
+    if result['layout'] == "meld": #Card has half-card on back, 'melds' with another
+        related_cards = result['all_parts']
+        name1 = related_cards[0]['name']
+        name2 = related_cards[1]['name']
+        name3 = related_cards[2]['name']
+        link1 = related_cards[0]['uri']
+        page1 = requests.get(link1)
+        image1 = page1.json()['image_uris']['normal']
+        link2 = related_cards[1]['uri']
+        page2 = requests.get(link2)
+        image2 = page2.json()['image_uris']['normal']
+        link3 = related_cards[2]['uri']
+        page3 = requests.get(link3)
+        image3 = page3.json()['image_uris']['normal']
+        print(image1)
+        print(image2)
+        print(image3)
+        album.append(InputMediaPhoto(image1, caption = name1))
+        album.append(InputMediaPhoto(image2, caption = name2))
+        album.append(InputMediaPhoto(image3, caption = name3))
+        return album
+    elif result['layout'] == "transform": #Card has full card on back
+        faces = result['card_faces']
+        name1 = faces[0]['name']
+        name2 = faces[1]['name']
+        image1 = faces[0]['image_uris']['normal']
+        image2 = faces[1]['image_uris']['normal']
+        print(image1)
+        print(image2)
+        album.append(InputMediaPhoto(image1, caption = name1))
+        album.append(InputMediaPhoto(image2, caption = name2))
+        return album
+    else:
+        image = result['image_uris']['normal']
+        name = result['name']
+        print(name)
+        print(image)
+        album.append(InputMediaPhoto(image, caption = name))
+        return album
 
 def card_image_search(bot, update): #Post results in chat
     searches = name_filter(update.message.text)
 
-    results = scryfall(searches)
-    album = results[0]
-    errors = results[1]
+    (album, errors) = scryfall(searches)
     print(album)
     print(errors)
     if len(errors) != 0:
         reply = "Not found: "
         for x in errors:
             update.message.reply_text(reply+x)
+    while len(album) > 10:
+        bot.send_media_group(chat_id = update.message.chat_id, media = album[0:10])
+        album = album[10:]
     bot.send_media_group(chat_id = update.message.chat_id, media = album)
     print("done")
     
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 #Eager Construct Telegram Bot
 
-This is a work in progress. Main functionality is complete. Currently working on deploying to Heroku.
+Search for a card by enclosing its name with [[ ]] in your message. You can search for multiple cards at once, in which case the search results will be grouped into an album. The message DOES NOT need to only contain bracketed terms: you may mention the card as part of a regular message.
+Example: What's so great about [[Saheeli]] and [[Felidar Guardian]]?
+
+You can also search for other characteristics using Scryfall's search syntax.
+Common options include:
+ Oracle text     o:
+ Type            t:
+ Color           c:
+ Color identity  id:
+ Cost            m:
+Example: [[o:"can't attack you" t:enchantment c:r]]"
+
+One last feature is the ability to display multiple results by adding the 'l:' operator in the search. The maximum number of results from one such search is 10.
